0191da24d4f5f5ffaff0c93bd41ef0245541cccf
Max Schmitt <max.andreas.schmitt@googlemail.com>
2017-06-12 21:44:31 +0200

    Removing LC_ALL fix

--- sinusbot_installer.sh
+++ sinusbot_installer.sh
@@ -571,13 +571,6 @@ else
   update-ca-certificates >/dev/null 2>&1
 fi
 
-if [[ $(dpkg -l locales | awk '{print $1}' | grep "ii") != "ii" ]]; then
-  yellowMessage "Fixing your locales. Setting them to en_US.UTF-8"!
-  apt-get -qq install locales -y >/dev/null 2>&1
-  locale-gen --purge en_US.UTF-8 >/dev/null 2>&1
-  echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\n' > /etc/default/locale
-fi
-
 greenMessage "Packages installed"!
 
 USERADD=$(which useradd)
