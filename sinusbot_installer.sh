481ab5fb0da3f250e8144a821c49681975208560
Philipp <philipp.esswein@me.com>
2017-06-12 19:46:56 +0200

    dBus fix
    https://github.com/Sinusbot/installer-linux/issues/12

--- sinusbot_installer.sh
+++ sinusbot_installer.sh
@@ -564,10 +564,10 @@ VERSION="3.0.19.4"
 magentaMessage "Installing necessary packages! Please wait..."
 
 if [ -f /etc/centos-release ]; then
-  yum -y -q install screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 curl less cron-apt ntp python iproute which >/dev/null 2>&1
+  yum -y -q install screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 curl less cron-apt ntp python iproute which dbus >/dev/null 2>&1
   update-ca-trust extract >/dev/null 2>&1
 else
-  apt-get -qq install screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 curl less cron-apt ntp python iproute2 -y >/dev/null 2>&1
+  apt-get -qq install screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 curl less cron-apt ntp python iproute2 dbus -y >/dev/null 2>&1
   update-ca-certificates >/dev/null 2>&1
 fi
 
