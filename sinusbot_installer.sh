1f1a8f5a02377581c23063c6f378cf5ae884b458
Philipp <philipp.esswein@me.com>
2017-06-26 19:18:31 +0200

    Cronjob fix2

--- sinusbot_installer.sh
+++ sinusbot_installer.sh
@@ -815,7 +815,7 @@ if [ "$YT" == "Yes" ]; then
     redMessage "Cronjob already set for YT-DL updater"!
   else
     greenMessage "Installing Cronjob for automatic YT-DL update..."
-    echo "0 0 * * * su $SINUSBOTUSER youtube-dl -U --restrict-filename >/dev/null 2>&1">>/etc/cron.d/ytdl
+    echo "0 0 * * * $SINUSBOTUSER youtube-dl -U --restrict-filename >/dev/null 2>&1">>/etc/cron.d/ytdl
     greenMessage "Installing Cronjob successful."
   fi
   
