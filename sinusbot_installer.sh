fe82c82a365178d36c418760dfff94082d634638
Max Schmitt <max@schmitt.mx>
2019-01-17 13:19:03 +0100

    fixed ts3 client download URL

--- sinusbot_installer.sh
+++ sinusbot_installer.sh
@@ -608,7 +608,7 @@ VERSION="3.2.3"
 
 #VERSION=$(wget -qO - 'https://sinusbot-installer.de/ts3version')
 
-DOWNLOAD_URL_VERSION="http://dl.4players.de/ts/releases/$VERSION/TeamSpeak3-Client-linux_$ARCH-$VERSION.run"
+DOWNLOAD_URL_VERSION="https://files.teamspeak-services.com/releases/client/$VERSION/TeamSpeak3-Client-linux_$ARCH-$VERSION.run"
  STATUS=$(wget --server-response -L $DOWNLOAD_URL_VERSION 2>&1 | awk '/^  HTTP/{print $2}')
   if [ "$STATUS" == "200" ]; then
     DOWNLOAD_URL=$DOWNLOAD_URL_VERSION
